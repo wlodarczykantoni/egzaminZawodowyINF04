f6ebc9d4dad88e54d43dc550ac88ed187baabaa3
wlodarczykantoni <xbbshshxhz@gmail.com>
2025-12-12 20:05:42 +0100

    Aplikacja skończona :)

--- egzaminWlasny0712/konsola0712.java
+++ egzaminWlasny0712/konsola0712.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class konsola0712 {
@@ -43,34 +44,60 @@ public class konsola0712 {
 			    "!", "@", "#", "$", "%"
 			));
 		
-		List<Integer>listaIntow = new ArrayList();
+		List<String>listaIntow = new ArrayList();
 		listaIntow.addAll(Arrays.asList(
-			   0,1,2,3,4,5,6,7,8,9
+			   "0","1","2","3","4","5","6","7","8","9"
 			));
 
 
-//login jedno z imienia i nazwisko male wszystko
-		char login = imie.charAt(0);
-	//	String password = 
-//testowe wywolanie
+//login jedno z imienia i nazwisko cale, ale male wszystko
+	String login = imie.toLowerCase().charAt(0)+nazwisko.toLowerCase();
+	System.out.println("Tutaj jest twój login: "+login);
+	System.out.println("Tutaj jest wygenerowane dla ciebie hasło: ");
 generowaniaHasla(iloscZnakowWHasle, listaMalychLiter, listaDuzychLiter, listaZnakow, listaIntow);
 	}
-	//TO-DO dokoncz generowanie hasla
-	public static void generowaniaHasla(int iloscZnakowWHasle, List<String>listaMalychLiter,List<String>listaDuzychLiter,List<String>listaZnakow,List<Integer>listaIntow ) {
+	
+	public static void generowaniaHasla(int iloscZnakowWHasle, List<String>listaMalychLiter,List<String>listaDuzychLiter,List<String>listaZnakow,List<String>listaIntow ) {
 		int i = 0;
+		Random random = new Random();
+		List<String>password = new ArrayList();
+														
 		while(i<iloscZnakowWHasle) {
-			Collections.shuffle(listaMalychLiter);
-			System.out.println(listaMalychLiter.get(0));
-			i++;
-			Collections.shuffle(listaDuzychLiter);
-			System.out.println(listaDuzychLiter.get(0));
-			i++;
-			Collections.shuffle(listaZnakow);
-			System.out.println(listaZnakow.get(0));
-			i++;
+			int resultRandom = random.nextInt(4);
+			if(resultRandom == 0) {
+				int b=0;
+			    Collections.shuffle(listaMalychLiter);
+			//    System.out.println(listaMalychLiter.get(0));
+			    password.add(listaMalychLiter.get(0));
+			    i++;
+			} else if(resultRandom == 1) {
+				int b=0;
+			    Collections.shuffle(listaDuzychLiter);
+			//    System.out.println(listaDuzychLiter.get(0));
+			    password.add(listaDuzychLiter.get(0));
+
+			    i++;
+			    b++;
+			}
+
+			else if(resultRandom == 2) {
+			    Collections.shuffle(listaZnakow);
+			//    System.out.println(listaZnakow.get(0));
+			    password.add(listaZnakow.get(0));
+			    i++;
+			    
+			} else if(resultRandom == 3) {
+			    Collections.shuffle(listaIntow);
+			//    System.out.println(listaIntow.get(0));
+			    password.add(listaIntow.get(0));
+			    i++;
+			}
 			
-		
          }
+		System.out.print("Hasło to: ");
+		for(int x = 0;x<password.size();x++) {
+			System.out.print(password.get(x));
+			}
 			
 			
 		}
